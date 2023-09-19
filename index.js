a15cabd68e769ef7dab08c051505b59b04a233ef
otienolivio <livio5261@gmail.com>
2023-09-19 07:06:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
-}
+  if (Feet <= 400) {return ('This one is on me!')}
+    else if (Feet>2000&&Feet<=2500){return ('I will gladly take your thirty bucks.')}
+    else {return ('No can do.')}
 
+}
 function ternaryCheckCity(){
-  // Write your code here!
+  if (ternaryCheckCity('NYC')) {return ("Ok, sounds good." )}
+  else if (ternaryCheckCity('Pittsburgh')) {return ('No go.')}
+
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  if (switchOnCharmFromTip('generous')) {return ('Thank you so much.')}
+  else if (switchOnCharmFromTip('not as generous')) {return ('Thank you.')}
+  else {return ('Bye.')}
 }
